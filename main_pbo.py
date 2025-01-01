0cf987c93e605ea62d70b35042ab81b2a18d1065
marisa2503 <marisahsn25@gmail.com>
2025-01-02 00:07:49 +0900

    Update main_pbo.py

--- main_pbo.py
+++ main_pbo.py
@@ -10,3 +10,17 @@ print ("\nTest Pesan : ", p.mes(), "\n")
 
 # silahkan uji fungsi dan class anda dibawah
 # pastikan class dan fungsi sudah anda buat di modul_satu_pbo.py
+from modul_satu_pbo import Buku
+
+
+# Membuat objek Buku
+buku1 = Buku("Laskar Pelangi", "Andrea Hirata", 2005, "Fiksi")
+buku2 = Buku("Atomic Habits", "James Clear", 2018, "Pengembangan Diri")
+
+# Menggunakan fungsi yang ada di dalam class Buku
+print(buku1.deskripsi_buku())
+print(buku2.deskripsi_buku())
+
+# Mengecek genre buku
+print(buku1.cek_genre("Fiksi"))
+print(buku2.cek_genre("Fiksi"))
