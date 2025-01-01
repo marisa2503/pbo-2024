756886be4d66dc1ae507fe8154911c4d0863da88
marisa2503 <marisahsn25@gmail.com>
2025-01-01 23:49:18 +0900

    Update modul_satu_pbo.py

--- modul_satu_pbo.py
+++ modul_satu_pbo.py
@@ -13,3 +13,18 @@ class test:
 
 # silahkan lanjutkan dengan fungsi dan calss anda dibawah
 # pastikan untuk menguji class dan fungsi yang sudah di buat disini
+class Buku:
+    def __init__(self, judul, penulis, tahun_terbit, genre):
+        self.judul = judul
+        self.penulis = penulis
+        self.tahun_terbit = tahun_terbit
+        self.genre = genre
+
+    def deskripsi_buku(self):
+        return f"'{self.judul}' adalah buku bergenre {self.genre} yang ditulis oleh {self.penulis} dan diterbitkan pada tahun {self.tahun_terbit}."
+
+    def cek_genre(self, genre):
+        if self.genre.lower() == genre.lower():
+            return f"Buku '{self.judul}' termasuk dalam genre {genre}."
+        else:
+            return f"Buku '{self.judul}' bukan termasuk dalam genre {genre}."
